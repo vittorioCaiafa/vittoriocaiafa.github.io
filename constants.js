bd8d3230775efa8a88afeca68b5eb6a2dc47a959
vittorio-caiafa <90295635+vittorioCaiafa@users.noreply.github.com>
2025-03-28 12:50:22 -0300

    export added to image url

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const flowerUrl =
+  "https://img.freepik.com/foto-gratis/primer-disparo-flor-morada_181624-25863.jpg?t=st=1743175252~exp=1743178852~hmac=c5dec0139e7a04f36cf1706cea97ed522c28dc58fd1654f515372880903beed0&w=1380";
