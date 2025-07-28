2eee86b7136ef2d4eeed487853982eb42df6ee44
vittorio-caiafa <90295635+vittorioCaiafa@users.noreply.github.com>
2025-07-27 21:24:05 -0300

    footer

--- components/footer.js
+++ components/footer.js
@@ -6,10 +6,24 @@ class Footer extends HTMLElement {
 
   connectedCallback() {
     this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="css/footer.css" />
       <footer>
-        <p>&copy; 2025 Vittorio Caiafa. Todos los derechos reservados.</p>
-      </footer>`;
+        <div class="footer-content">
+          <div class="copyright">
+            Copyright &copy; 2025 Vittorio Caiafa
+          </div>
+          <div class="social-icons">
+            <a href="https://www.linkedin.com" target="_blank" title="LinkedIn"><img
+            src="assets/icons/socials/linkedin.png" /></a>
+            <a href="https://github.com" target="_blank" title="GitHub"><img
+            src="assets/icons/socials/github.png" /></a>
+            <a href="https://www.instagram.com" target="_blank" title="Instagram"><img
+            src="assets/icons/socials/instagram.png" /></a>
+          </div>
+        </div>
+      </footer>
+    `;
   }
 }
 
-customElements.define("Footer", Footer);
+customElements.define("footer-comp", Footer);
